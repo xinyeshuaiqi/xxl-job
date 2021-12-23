9881c5683eb1ad05b4fe35aea9cbdebc41b4b345
wangmingxin03 <wangmingxin03@kuaishou.com>
2021-12-23 21:34:54 +0800

    up

---
+++ src/main/java/pers/wmx/demo/DemoApplication.java
@@ -0,0 +1,13 @@
+package pers.wmx.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DemoApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DemoApplication.class, args);
+    }
+
+}
